3338641c8f2e2d2a6fe5f3c633a01486542be5d1
estre <estre@ALEXANDRAAOG>
2024-04-26 11:06:57 -0600

    lo

---
+++ ITD.PERROSPERDIDOS.INFRAESTRUCTURE/REPOSITORIES/prueba.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITD.PERROSPERDIDOS.INFRAESTRUCTURE.REPOSITORIES
+{
+    internal class prueba
+    {
+    }
+}
